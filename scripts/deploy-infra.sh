756a04e728892a0a05ccb437e11278761fdd7d0f
deeep08 <deeep0810@gmail.com>
2020-06-30 14:21:58 +0100

    Added Serverless API endpoint and DNS

--- scripts/deploy-infra.sh
+++ scripts/deploy-infra.sh
@@ -3,9 +3,16 @@ STACK_NAME=awslambdasample
 REGION=us-east-1
 CLI_PROFILE=deeep08
 
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text)
+# AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text)
 
-WEBSITE_BUCKET="$STACK_NAME-website-$REGION-$AWS_ACCOUNT_ID"
+API_STAGE_NAME="prod"
+
+DOMAIN=deeep08.com
+SUBDOMAIN=$STACK_NAME
+
+WEBSITE_BUCKET="$SUBDOMAIN.$DOMAIN"
+BUCKET_DNS_NAME="s3-website-us-east-1.amazonaws.com"
+BUCKET_HOSTED_ZONE_ID="Z3AQBSTGFYJSTF"
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -17,15 +24,30 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-      WebsiteBucketName="$WEBSITE_BUCKET"
-
-aws --profile $CLI_PROFILE s3 cp ../html/index.html s3://$WEBSITE_BUCKET
-aws --profile $CLI_PROFILE s3 cp ../html/error.html s3://$WEBSITE_BUCKET
-aws --profile $CLI_PROFILE s3api put-object-acl --bucket $WEBSITE_BUCKET --key index.html --acl public-read
-aws --profile $CLI_PROFILE s3api put-object-acl --bucket $WEBSITE_BUCKET --key error.html --acl public-read
+      WebsiteBucketName="$WEBSITE_BUCKET" \
+      ApiStageName="$API_STAGE_NAME" \
+      Domain="$DOMAIN" \
+      SubDomain="$SUBDOMAIN" \
+      WebsiteBucketDNSName="$BUCKET_DNS_NAME" \
+      WebsiteBucketHostedZoneId="$BUCKET_HOSTED_ZONE_ID"
 
 # If the deploy succeeded, show the DNS name of the created bucket
 if [ $? -eq 0 ]; then
+  API_GATEWAY_ID=$(aws cloudformation list-exports \
+    --profile $CLI_PROFILE \
+    --query "Exports[?Name=='ApiGatewayID'].Value" --output text)
+
+  API_GATEWAY_ENDPOINT="https://$API_GATEWAY_ID.execute-api.$REGION.amazonaws.com/$API_STAGE_NAME/"
+
+  sed "s|PUT_API_GATEWAY_ENDPOINT_HERE|$API_GATEWAY_ENDPOINT|g" ../html/index.html > index_tmp.html
+
+  aws --profile $CLI_PROFILE s3 cp index_tmp.html s3://$WEBSITE_BUCKET/index.html
+  aws --profile $CLI_PROFILE s3 cp ../html/error.html s3://$WEBSITE_BUCKET
+  aws --profile $CLI_PROFILE s3api put-object-acl --bucket $WEBSITE_BUCKET --key index.html --acl public-read
+  aws --profile $CLI_PROFILE s3api put-object-acl --bucket $WEBSITE_BUCKET --key error.html --acl public-read
+
+  rm -f index_tmp.html
+
   aws cloudformation list-exports \
     --profile $CLI_PROFILE \
     --query "Exports[?ends_with(Name, 'Endpoint')].Value"
--- scripts/remove-infra.sh
+++ scripts/remove-infra.sh
@@ -3,9 +3,19 @@ STACK_NAME=awslambdasample
 REGION=us-east-1
 CLI_PROFILE=deeep08
 
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text)
+DOMAIN=deeep08.com
+SUBDOMAIN=$STACK_NAME
 
-WEBSITE_BUCKET="$STACK_NAME-website-$REGION-$AWS_ACCOUNT_ID"
+WEBSITE_BUCKET="$SUBDOMAIN.$DOMAIN"
 
+# Empty the website bucket
+echo -e "\n\n=========== Emptying website bucket ==========="
 aws --profile $CLI_PROFILE s3 rm s3://$WEBSITE_BUCKET/index.html
 aws --profile $CLI_PROFILE s3 rm s3://$WEBSITE_BUCKET/error.html
+
+# Delete the website stack
+echo -e "\n\n=========== Deleting the website stack ==========="
+aws cloudformation delete-stack \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME
